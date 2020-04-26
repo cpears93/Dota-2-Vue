0395423e6e1db87d5c22d1ac8420e7a53d87a0eb
Christopher Pearson <cpearson3140@gmail.com>
2020-04-26 14:24:07 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,70 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <dota2-header title="Dota2" />
+    <dota2-header v-if="!heroes.length" title="LOADING..." />
+    <div id="list-info" v-if="heroes.length">
+      <vuedog-beer-list :beers="beers"></vuedog-beer-list>
+      <vuedog-beer-info
+        v-if="selectedBeer"
+        :beer="selectedBeer"
+      ></vuedog-beer-info>
+    </div>
+    <vuedog-favourites-list :favourites="favourites"></vuedog-favourites-list>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { eventBus } from "@/main.js";
+import Dota2Heroes from "@/components/Dota2Heroes";
+import Dota2Matches from "@/components/Dota2Matches";
+import Dota2Rankings from "@/components/Dota2Rankings";
 
 export default {
-  name: 'App',
+  name: "app",
   components: {
-    HelloWorld
+    "dota2-header": Dota2Header,
+    "dota2-matches": Dota2Matches,
+    "dota2-list": Dota2Rankings
+  },
+  data() {
+    return {
+      matches: [],
+      selectedBeer: null
+    };
+  },
+  computed: {
+    favourites: function() {
+      return this.matches.filter(matches => matches.isFavourite);
+    }
+  },
+  methods: {
+    getBeers: function() {
+      fetch("https://api.punkapi.com/v2/beers")
+        .then(res => res.json())
+        .then(beerData => {
+          beerData.forEach(beer => (beer.isFavourite = false));
+          this.beers = beerData;
+        })
+        .then(() => this.sortBeers("name"));
+    },
+    sortBeers: function(property) {
+      this.beers.sort((a, b) => {
+        return a[property] < b[property] ? -1 : 1;
+      });
+    },
+    markFavourite: function(beer) {
+      const index = this.beers.indexOf(beer);
+      this.beers[index].isFavourite = true;
+    }
+  },
+  mounted() {
+    this.getBeers();
+
+    eventBus.$on("beer-selected", beer => (this.selectedBeer = beer));
+
+    eventBus.$on("favourite-added", beer => this.markFavourite(beer));
   }
-}
+};
 </script>
 
 <style>
